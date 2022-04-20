568e82a5f9363ee0b97b1343070d5ccb38b89eab
Eugene Zaukin <ezapach@yandex.ru>
2022-04-20 22:27:43 +0300

    Redraw layout on fragments

--- app/src/main/java/com/eugene/calculator/MainActivity.java
+++ app/src/main/java/com/eugene/calculator/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -46,9 +45,9 @@ public class MainActivity extends ActivitySettings implements View.OnClickListen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getAppTheme();
+//        getAppTheme();
         setContentView(R.layout.activity_main);
-        initView();
+//        initView();
     }
 
     private void initView() {
