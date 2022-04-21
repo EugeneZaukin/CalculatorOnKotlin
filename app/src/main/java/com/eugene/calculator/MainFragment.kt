c455f1f0280e0aea12be11446ce692cb751b2dd1
Eugene Zaukin <ezapach@yandex.ru>
2022-04-21 15:45:39 +0300

    Add logic display button text

--- app/src/main/java/com/eugene/calculator/MainFragment.kt
+++ app/src/main/java/com/eugene/calculator/MainFragment.kt
@@ -3,7 +3,9 @@ package com.eugene.calculator
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.*
+import androidx.lifecycle.lifecycleScope
 import com.eugene.calculator.databinding.MainFragmentBinding
+import kotlinx.coroutines.flow.*
 
 class MainFragment : Fragment() {
     private var _binding: MainFragmentBinding? = null
@@ -21,21 +23,32 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        startObserve()
         initCalculatorButtons()
+    }
+
+    private fun startObserve() {
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.result
+                .onEach {
+                    binding.textView2.text = it
+                }
+                .collect()
+        }
     }
 
     private fun initCalculatorButtons() {
         with(binding) {
-            buttonZero.setOnClickListener {  }
-            button1.setOnClickListener {  }
-            button2.setOnClickListener {  }
-            button3.setOnClickListener {  }
-            button4.setOnClickListener {  }
-            button5.setOnClickListener {  }
-            button6.setOnClickListener {  }
-            button7.setOnClickListener {  }
-            button8.setOnClickListener {  }
-            button9.setOnClickListener {  }
+            buttonZero.setOnClickListener { viewModel.addButtonClick(buttonZero.text.toString()) }
+            button1.setOnClickListener { viewModel.addButtonClick(button1.text.toString()) }
+            button2.setOnClickListener { viewModel.addButtonClick(button2.text.toString()) }
+            button3.setOnClickListener { viewModel.addButtonClick(button3.text.toString()) }
+            button4.setOnClickListener { viewModel.addButtonClick(button4.text.toString()) }
+            button5.setOnClickListener { viewModel.addButtonClick(button5.text.toString()) }
+            button6.setOnClickListener { viewModel.addButtonClick(button6.text.toString()) }
+            button7.setOnClickListener { viewModel.addButtonClick(button7.text.toString()) }
+            button8.setOnClickListener { viewModel.addButtonClick(button8.text.toString()) }
+            button9.setOnClickListener { viewModel.addButtonClick(button9.text.toString()) }
         }
     }
 }
--- app/src/main/java/com/eugene/calculator/MainViewModel.kt
+++ app/src/main/java/com/eugene/calculator/MainViewModel.kt
@@ -1,7 +1,16 @@
 package com.eugene.calculator
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.*
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val stringBuilder = StringBuilder()
+
+    private var _result = MutableSharedFlow<StringBuilder>(1, 0, BufferOverflow.DROP_OLDEST)
+    val result get() = _result.asSharedFlow()
+
+    fun addButtonClick(textOnButton: String) {
+        _result.tryEmit(stringBuilder.append(textOnButton))
+    }
 }
